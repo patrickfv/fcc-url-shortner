482627a2d0fcf90eed868b381eaa2701c6711462
patrickfv <patrick.fv@outlook.com>
2020-11-08 16:26:15 +0000

    config database

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,11 +15,19 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const urlSchema = new mongoose.Schema({
+  original: { type: String, required: true },
+  short: Number
+});
+
+const url = mongoose.model('Url', urlSchema);
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
-
 
 
 app.listen(port, function() {
