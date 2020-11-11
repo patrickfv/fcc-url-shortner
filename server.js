4c019b49b0e2124106d0aad13f1f37999f6d2bca
patrickfv <patrick.fv@outlook.com>
2020-11-11 01:58:26 +0000

    refactor and fix test running

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', function(req, res) {
@@ -25,32 +27,7 @@ const urlSchema = new mongoose.Schema({
 });
 
 const Url = mongoose.model('Url', urlSchema);
-//callback - if pass invalid url
-const dnsCallback = (resolve, reject) => (err, address) => {
-  if(err) reject({ error: 'invalid url' });
 
-  resolve({ address });
-}
-//Promise - 
-function verifyURL(url) {
-  url = url.replace('https://', '').replace('http://', '');
-  return new Promise((resolve, reject) => {
-    dns.lookup(url, dnsCallback(resolve, reject));
-  });
-}
-/*
-function formatURL(url) {
-  if(url.includes('https://www.')) {
-     return url;
-  } else if(url.includes('www.')) {
-    return `https://${url}`;
-  } else if(url.includes('https://')) {
-    return url.replace('https://', 'https://www.');
-  } else {
-    return `https://www.${url}`;
-  }
-}
-*/
 function findByShort(number, callback) {
   return Url.findOne({ short: number }, callback);
 }
@@ -68,16 +45,6 @@ async function findAndCreateByURL(find) {
     Url.create(docCreate);
     return docCreate;
   }
-  /*
-const doc = await Url.findOneAndUpdate(find, { short: ++lenght }, {
-    'new': true,
-    upsert: true
-  })
-    .sort(filter)
-    .exec((err, result) => {
-      console.log(err, result)
-    });
-  */
   return doc;
 }
 
@@ -86,21 +53,24 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.post('/api/shorturl/new', bodyParser.urlencoded({ extended: false }), (req, res) => {
+app.post('/api/shorturl/new', (req, res) => {
   const { url } = req.body;
-  
-  verifyURL(url)
-    .then( async result => {
-      const doc = await findAndCreateByURL({ original: url });
-      
-      res.json({
-        original_url: doc.original,
-        short_url: doc.short
-      });
-    })
-    .catch( err => {
-      res.json(err);
+  if(!url) {
+    return;
+  }
+  const filter = url.replace('https://', '').replace('http://', '');
+  dns.lookup(filter, async (err, address, family) => {
+    if(err && !err.hostname.includes('timestamp')) {
+      return res.json({ error: 'invalid url'});
+    }
+    const doc = await findAndCreateByURL({ original: url });
+    return res.json({
+      original_url: doc.original,
+      short_url: doc.short
     });
+
+  });
+
 });
 
 app.get('/api/shorturl/:short', (req, res) => {
@@ -113,7 +83,7 @@ app.get('/api/shorturl/:short', (req, res) => {
       res.send('URL not Found');
     }
   });
-    
+
 });
 
 app.listen(port, function() {
